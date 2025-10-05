9483d91f7bfb56187fcc53ee2adc76469afd5723
Ripudaman Singh <rdsingh120@gmail.com>
2025-10-05 14:15:51 +0530

    update qr code

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,14 +1,15 @@
 import { BsChevronCompactRight } from 'react-icons/bs'
+import qr from '../assets/qr.png'
 
 const Contact = () => {
   return (
     <div
       className="text-white min-h-[16.66%] rounded-2xl p-2 lg:p-5 border border-gray-500 
-      flex flex-col items-center justify-center transform transition-transform duration-300 hover:scale-95 md:hover:scale-105"
+      flex flex-col items-center justify-center transform transition-transform duration-300  hover:scale-95 md:hover:scale-105"
     >
       <div className="rounded-2xl p-1 lg:p-3 border border-white w-full flex justify-between items-center">
         <img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/QR_Code_Example.svg/1200px-QR_Code_Example.svg.png"
+          src={qr}
           alt=""
           className="bg-white rounded w-20 md:w-10 lg:w-20"
         />
