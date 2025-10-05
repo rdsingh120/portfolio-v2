936736659806caca1529e9c8014b92697063cb44
Ripudaman Singh <rdsingh120@gmail.com>
2025-10-05 13:28:54 +0530

    Update Profile.jsx

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -3,7 +3,7 @@ import tag from '../assets/icons/tag.png'
 const Profile = () => {
   return (
     <div
-      className="bg-[#bbf2ff] rounded-2xl p-5 pt-10 md:p-2 md:pt-5 lg:p-5 lg:pt-5 border-b-2 border-dashed 
+      className="bg-[#bbf2ff] rounded-2xl p-5 pt-10 md:pt-5 lg:p-5 lg:pt-5 border-b-2 border-dashed 
     border-black flex flex-col items-center justify-between transform transition-transform duration-300
             hover:scale-105"
     >
@@ -18,11 +18,11 @@ const Profile = () => {
         <h1 className="text-2xl md:text-lg lg:text-2xl font-medium text-center">
           Ripudaman Singh
         </h1>
-        <h3 className="text-center text-base md:text-sm lg:text-base">
+        <h3 className="text-center text-base md:text-xs lg:text-base">
           Software Engineer in Toronto
         </h3>
       </div>
-      <div className="rounded-2xl bg-[#eb5a35] w-full p-5 md:p-2 lg:p-5 flex items-center justify-center">
+      <div className="rounded-2xl bg-[#eb5a35] w-full p-5 md:p-3 lg:p-5 flex items-center justify-center">
         <img
           className="rounded-xl w-full h-full object-cover object-right"
           src={photo}
