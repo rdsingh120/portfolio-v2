9cd33d22388ff71923c1a77201b4e32c7f378be5
Ripudaman Singh <rdsingh120@gmail.com>
2025-10-05 12:47:20 +0530

    Update Blog.jsx

--- client/src/components/Blog.jsx
+++ client/src/components/Blog.jsx
@@ -1,12 +1,22 @@
-import BlogPost from "./BlogPost"
-import Project from "./Project"
+import { PiLinkSimple } from 'react-icons/pi'
+
+import BlogPost from './BlogPost'
 
 const Blog = () => {
   return (
     <div className="bg-[#4085e7] text-white rounded-2xl p-2 lg:p-5 mb-0 md:mb-0 lg:mb-2">
-      <h3 className="font-medium text-center lg:text-left text-xl md:text-lg lg:text-xl   mb-2">
-        Blogs
-      </h3>
+      <div className="flex justify-between items-center">
+        <h3 className="font-medium text-center lg:text-left text-xl md:text-lg lg:text-xl   mb-2">
+          Blogs
+        </h3>
+        <a
+          href="https://blog.rdsingh.dev"
+          className="bg-[#70a3ed] rounded-xl px-4 flex items-center gap-1"
+        >
+          <PiLinkSimple/>
+          <span className="text-sm underline">blog.rdsingh.dev</span>
+        </a>
+      </div>
       <div className="flex gap-1">
         <BlogPost />
         <BlogPost />
